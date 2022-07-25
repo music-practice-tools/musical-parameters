081963b27a4ea889f1a7e1e879872bfa472f5941
Steve Lee <steve@opendirective.com>
2022-07-25 01:08:06 +0100

    remove id

--- src/parameter-picker.js
+++ src/parameter-picker.js
@@ -1,13 +1,13 @@
 import { pickRandom } from "./random.js";
 
-export function createParameterPicker({ id, name, values }) {
-  const render = (element, { id, name, value }) => {
-    element.innerHTML = `<div data-id="${id}" class="picker"><div class="picker-name">${name}:</div><div class="picker-value">${value}</div><button>\u{1F504}</button></div>`;
+export function createParameterPicker({ name, values }) {
+  const render = (element, { name, value }) => {
+    element.innerHTML = `<div class="picker"><div class="picker-name">${name}:</div><div class="picker-value">${value}</div><button>\u{1F504}</button></div>`;
   };
   const element = document.createElement("div");
   const onClick = (_) => {
     const value = pickRandom(values);
-    render(element, { id, name, value });
+    render(element, { name, value });
   };
   element.addEventListener("click", onClick);
   onClick();
